013e90e02baaf148b9a24a71ed220b9cf73ccd3c
Sashakrm <sasakramch@gmail.com>
2025-03-03 21:52:16 +0200

    Add files via upload

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp10
+namespace ConsoleApp11
 {
     class Program
     {
@@ -13,10 +13,28 @@ namespace ConsoleApp10
             int arraySize = InputIntNum("Array Size : ");
             int[] array = new int[arraySize];
             InputIntMasiv(array);
-            Array.Reverse(array);
+            //SortIntMas(array);
+            Array.Sort(array);
             OutIntMas(array);
             Console.ReadLine();
 
+        }
+        static void SortIntMas(int[] omas)
+        {
+            int transfer;
+            for (int i = 0; i < omas.Length - 1; i++)
+            {
+                for (int t = i + 1; t < omas.Length; t++)
+                {
+                    if (omas[i] < omas[t])
+                    {
+
+                        transfer = omas[i];
+                        omas[i] = omas[t];
+                        omas[t] = transfer;
+                    }
+                }
+            }
         }
         static int InputIntNum(string message)
         {
@@ -25,7 +43,7 @@ namespace ConsoleApp10
         }
         static void InputIntMasiv(int[] omas)
         {
-            for(int i = 0; i < omas.Length; i++)
+            for (int i = 0; i < omas.Length; i++)
             {
                 omas[i] = InputIntNum($"omas{i} : ");
             }
@@ -33,7 +51,7 @@ namespace ConsoleApp10
         static void OutIntMas(int[] omas)
         {
             Console.WriteLine("вывод :");
-            for(int i = omas.Length -1; i >= 0; i--)
+            for (int i = omas.Length - 1; i >= 0; i--)
             {
                 Console.WriteLine($"Omas[{i}] = {omas[i]}");
             }
