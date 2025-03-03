b92afa7d6d62ad6ebd0b41e06b5724bd6eea9068
Sashakrm <sasakramch@gmail.com>
2025-03-03 21:41:29 +0200

    Task 1

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int arraySize = InputIntNum("Array Size : ");
+            int[] array = new int[arraySize];
+            InputIntMasiv(array);
+            Array.Reverse(array);
+            OutIntMas(array);
+            Console.ReadLine();
+
+        }
+        static int InputIntNum(string message)
+        {
+            Console.Write(message);
+            return int.Parse(Console.ReadLine());
+        }
+        static void InputIntMasiv(int[] omas)
+        {
+            for(int i = 0; i < omas.Length; i++)
+            {
+                omas[i] = InputIntNum($"omas{i} : ");
+            }
+        }
+        static void OutIntMas(int[] omas)
+        {
+            Console.WriteLine("вывод :");
+            for(int i = omas.Length -1; i >= 0; i--)
+            {
+                Console.WriteLine($"Omas[{i}] = {omas[i]}");
+            }
+        }
+    }
+}
